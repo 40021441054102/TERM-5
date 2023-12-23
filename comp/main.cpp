1b4529bab60d45f1955dc2dae6d3c4eae046a3b6
RamtinKosari <ramtinkosari@gmail.com>
2023-12-23 23:12:10 +0330

    [#REFACTOR] Update Codes

--- comp/main.cpp
+++ comp/main.cpp
@@ -30,56 +30,56 @@ bool isspace(char c) {
     return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v';
 }
 
-// std::istringstream iss;
+std::istringstream iss;
 
-// bool match(const std::string& token) {
-//     std::string line;
-//     std::getline(iss, line);
-//     return line.find(token) != std::string::npos;
-// }
+bool match(const std::string& token) {
+    std::string line;
+    std::getline(iss, line);
+    return line.find(token) != std::string::npos;
+}
 
-// bool parse_num() {
-//     std::string token;
-//     iss >> token;
-//     for (char c : token) {
-//         if (!isdigit(c) && c != '-') {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+bool parse_num() {
+    std::string token;
+    iss >> token;
+    for (char c : token) {
+        if (!isdigit(c) && c != '-') {
+            return false;
+        }
+    }
+    return true;
+}
 
-// bool parse_type() {
-//     return match("char") || match("real") || match("integer");
-// }
+bool parse_type() {
+    return match("char") || match("real") || match("integer");
+}
 
-// bool parse_array();
+bool parse_array();
 
-// bool parse_array_inner() {
-//     return match("[") && parse_num() && match("..") && parse_num() && match("]") && match("of") && (parse_type() || parse_array());
-// }
+bool parse_array_inner() {
+    return match("[") && parse_num() && match("..") && parse_num() && match("]") && match("of") && (parse_type() || parse_array());
+}
 
-// bool parse_array() {
-//     return match("array") && parse_array_inner();
-// }
+bool parse_array() {
+    return match("array") && parse_array_inner();
+}
 
-// bool parse() {
-//     return parse_array() && iss.eof();
-// }
+bool parse() {
+    return parse_array() && iss.eof();
+}
 
-// int main() {
-//     std::string line;
-//     while (std::getline(std::cin, line)) {
-//         iss.str(line);
-//         iss.clear();
-//         if (parse()) {
-//             std::cout << "parse successful!\n";
-//         } else {
-//             std::cout << "parse failure!\n";
-//         }
-//     }
-//     return 0;
-// }
+int main() {
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        iss.str(line);
+        iss.clear();
+        if (parse()) {
+            std::cout << "parse successful!\n";
+        } else {
+            std::cout << "parse failure!\n";
+        }
+    }
+    return 0;
+}
 
 int main() {
     char _char;
